3fffe694b7a153319f92e09c79fbe5060d348fd6
rachelkunowski <79378931+rachelkunowski@users.noreply.github.com>
2021-05-26 14:21:19 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://sarayupai.github.io/ewh-portable-microscopy/api'
+  API_ENDPOINT: 'https://rachelkunowski.github.io/microscopy/api'
   //API_ENDPOINT: 'http://localhost:3000/ewh-portable-microscopy/api',
 };
